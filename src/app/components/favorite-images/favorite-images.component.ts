2e290617210d9124388d683bbff3e717fb37f88c
SebasRojasM1 <Sebasrojasm1@gmail.com>
2024-09-12 09:15:31 -0500

    The modal was implemented in the favoritePage component succesfully

--- src/app/components/favorite-images/favorite-images.component.ts
+++ src/app/components/favorite-images/favorite-images.component.ts
@@ -9,6 +9,8 @@ import { ImageService } from 'src/app/services/image.service';
 })
 export class FavoriteImagesComponent implements OnInit {
   favoriteImages: Image[] = [];
+  isModalVisible = false; // Controla la visibilidad del modal
+  selectedImage: Image | null = null; // Almacena la imagen seleccionada para mostrar en el modal
 
   constructor(private imageService: ImageService) {}
 
@@ -18,5 +20,15 @@ export class FavoriteImagesComponent implements OnInit {
 
   getFavoriteImages(): void {
     this.favoriteImages = this.imageService.getFavoriteImages(); // Obtener las imágenes favoritas del servicio
+  }
+
+  openModal(image: Image) {
+    this.selectedImage = image; // Setea la imagen seleccionada
+    this.isModalVisible = true; // Muestra el modal
+  }
+
+  closeModal() {
+    this.isModalVisible = false; // Oculta el modal
+    this.selectedImage = null; // Limpia la imagen seleccionada
   }
 }
